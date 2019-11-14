cd3f7693d30cd39100b25a329a64984cdb7ae5e2
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2019-11-14 10:43:16 -0800

    Add functional tests for the two schedulers

---
+++ tests/test_scheduled_external_program.py
@@ -0,0 +1,17 @@
+import os
+import luigi
+from bioluigi.scheduled_external_program import ScheduledExternalProgramTask
+from distutils.spawn import find_executable
+from nose.plugins.skip import SkipTest
+
+class MyTask(ScheduledExternalProgramTask):
+    def program_args(self):
+        return ['true']
+
+def test_local_scheduler():
+    luigi.build([MyTask(scheduler='local')], local_scheduler=True)
+
+def test_slurm_scheduler():
+    if find_executable('srun') is None:
+        raise SkipTest('srun is needed to run Slurm tests.')
+    luigi.build([MyTask(scheduler='slurm')], local_scheduler=True)
