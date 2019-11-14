5b6f11b3f35697f1a2c5a9cd58099a234e9e0f20
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2019-11-14 10:01:53 -0800

    Remove tabix since we use bcftools index

--- bioluigi/config.py
+++ bioluigi/config.py
@@ -3,6 +3,5 @@ import luigi
 class bioluigi(luigi.Config):
     star_bin = luigi.Parameter(default='STAR')
     bcftools_bin = luigi.Parameter(default='bcftools')
-    tabix_bin = luigi.Parameter(default='tabix')
     vep_bin = luigi.Parameter(default='vep')
     vep_cache_dir = luigi.OptionalParameter(default='')
