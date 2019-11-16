73af4c172a5980d4d631a37f62a278371523ad0f
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2019-11-15 16:06:38 -0800

    Add FastQC task

--- bioluigi/config.py
+++ bioluigi/config.py
@@ -1,6 +1,7 @@
 import luigi
 
 class bioluigi(luigi.Config):
+    fastqc_bin = luigi.Parameter(default='fastqc')
     star_bin = luigi.Parameter(default='STAR')
     bcftools_bin = luigi.Parameter(default='bcftools')
     vep_bin = luigi.Parameter(default='vep')
---
+++ bioluigi/tasks/fastqc.py
@@ -0,0 +1,30 @@
+import datetime
+from os.path import join, basename, splitext
+import luigi
+from ..scheduled_external_program import ScheduledExternalProgramTask
+from ..config import bioluigi
+
+cfg = bioluigi()
+
+class GenerateReport(ScheduledExternalProgramTask):
+    input_file = luigi.Parameter()
+    output_dir = luigi.Parameter()
+
+    walltime = datetime.timedelta(minutes=30)
+    cpus = 1
+    memory = 2
+
+    @staticmethod
+    def gen_report_basename(fastq_path):
+        sample_name, ext = splitext(basename(fastq_path))
+        if ext == 'gz':
+            sample_name, ext = splitext(sample_name)
+        return '{}_fastqc.html'.format(sample_name)
+
+    def program_args(self):
+        return [cfg.fastqc_bin,
+                '--outdir', self.output_dir,
+                self.input_file]
+
+    def output(self):
+        return luigi.LocalTarget(join(self.output_dir, self.gen_report_basename(self.input_file)))
