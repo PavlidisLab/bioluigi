f63251bb8735bd7fc9d228a9cbb8ca82f2e0ba26
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2019-11-21 09:43:49 -0800

    Add cutadapt tasks for trimming single end and paired reads

--- bioluigi/config.py
+++ bioluigi/config.py
@@ -2,6 +2,8 @@ import luigi
 
 class bioluigi(luigi.Config):
     scheduler = luigi.Parameter(default='local', significant=False, description='Default scheduler to use in ScheduledExternalProgram')
+    prefetch_bin = luigi.Parameter(default='prefetch')
+    cutadapt_bin = luigi.Parameter(default='cutadapt')
     fastqc_bin = luigi.Parameter(default='fastqc')
     star_bin = luigi.Parameter(default='STAR')
     bcftools_bin = luigi.Parameter(default='bcftools')
--- bioluigi/tasks/cutadapt.py
+++ bioluigi/tasks/cutadapt.py
@@ -1,2 +1,66 @@
-class CutAdapters(ExternalProgramTask):
-    pass
+import datetime
+
+import luigi
+from ..scheduled_external_program import ScheduledExternalProgramTask
+
+class CutadaptTask(ScheduledExternalProgramTask):
+    """
+    Base class for all cutadapt-derived tasks.
+    """
+    task_namespace = 'cutadapt'
+
+    adapter_3prime = luigi.OptionalParameter(default='', positional=False)
+    adapter_5prime = luigi.OptionalParameter(default='', positional=False)
+
+    trim_n = luigi.BoolParameter(default=False, positional=False)
+    minimum_length = luigi.IntParameter(default=0, positional=False)
+
+    walltime = datetime.timedelta(hours=2)
+
+    def program_args(self):
+        args = [cfg.cutadapt_bin]
+
+        args.extend(['-j', self.cpus])
+
+        if self.adapter_3prime:
+            args.extend(['-a', self.adapter_3primer])
+
+        if self.adapter_5prime:
+            args.extend(['-g', self.adapter_5primer])
+
+        if self.trim_n:
+            args.append('--trim-n')
+
+        if self.minimum_length:
+            args.extend(['--minimum-length', self.minimum_length])
+
+        return args
+
+class TrimReads(CutadaptTask):
+    input_file =  luigi.Parameter()
+    output_file = luigi.Parameter()
+
+    def program_args(self):
+        args = super(TrimReads, self).program_args()
+        args.extend(['-o', self.output_file, self.input_file])
+        return args
+
+    def output(self):
+        return luigi.LocalTarget(self.output_file)
+
+class TrimPairedReads(CutadaptTask):
+    input_file = luigi.Parameter()
+    input2_file = luigi.Parameter()
+    output_file = luigi.Parameter()
+    output2_file = luigi.Parameter()
+
+    def program_args(self):
+        args = super(TrimReads, self).program_args()
+        args.extend([
+            '-o', self.output_file,
+            '-p', self.output2_file,
+            self.input_file, self.input2_file])
+        return args
+
+    def output(self):
+        return [luigi.LocalTarget(self.output_file), luigi.LocalTarget(self.output2_file)]
