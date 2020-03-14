19877958b2a96f2884d93779790ae0765908c5e3
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2020-03-14 12:00:43 -0700

    sratoolkit: Fix tests for prefetch and fastq-dump resources

--- tests/test_sratoolkit.py
+++ tests/test_sratoolkit.py
@@ -4,7 +4,8 @@ def test_prefetch():
     prefetch_task = sratoolkit.Prefetch('srr_accession', 'outdir')
     args = prefetch_task.program_args()
     assert 'cpus' in prefetch_task.resources
-    assert 'sra_http_connections' in prefetch_task.resources
+    assert 'prefetch_jobs' in prefetch_task.resources
+    assert prefetch_task.resources['prefetch_jobs'] == 1
 
 def test_fastq_dump():
     fastq_dump_task = sratoolkit.FastqDump('srr_accession', 'outdir', minimum_read_length=18)
@@ -14,3 +15,5 @@ def test_fastq_dump():
     assert args[-3] == '--outdir'
     assert args[-2].startswith('outdir-tmp')
     assert args[-1] == 'srr_accession'
+    assert 'fastq_dump_jobs' in fastq_dump_task.resources
+    assert fastq_dump_task.resources['fastq_dump_jobs'] == 1
