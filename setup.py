f9026a86539969742511cdbd1b95df112d1143be
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2021-06-18 13:53:20 -0700

    Bump patch version to 0.0.5

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='bioluigi',
-      version='0.0.3',
+      version='0.0.5',
       description='Reusable and maintained Luigi tasks to incorporate in bioinformatics pipelines',
       long_description='file: README.md',
       author='Guillaume Poirier-Morency',
