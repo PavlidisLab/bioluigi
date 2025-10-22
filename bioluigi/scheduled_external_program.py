ca5886cc6d0adc2e706a8b5127f114c3d705d304
Guillaume Poirier-Morency <guillaumepoiriermorency@gmail.com>
2025-10-21 19:07:23 -0700

    Add a local and ssh scheduler definitions
    Make srun binary configurable
    
    Add individual configuration sections for each scheduler.
    
    Add a test configuration to use for the CI.

--- bioluigi/scheduled_external_program.py
+++ bioluigi/scheduled_external_program.py
@@ -2,38 +2,38 @@
 This module provides a flavour of :class:`luigi.contrib.external_program.ExternalProgramTask`
 designed to work with an external scheduler such as Slurm.
 
-It also extend the basic definition of an external task to declare basic
+It also extends the basic definition of an external task to declare basic
 resource consumptions such as the number of CPU and the amount of memory
 required to execute the task.
 """
 
 import datetime
+import os
 from datetime import timedelta
 from typing import Optional
 
 import luigi
-from luigi.contrib.external_program import ExternalProgramTask
 
 from .config import bioluigi
 from .schedulers import get_available_schedulers, get_scheduler, ScheduledTask
 
 cfg = bioluigi()
 
-class ScheduledExternalProgramTask(ExternalProgramTask, ScheduledTask):
+class ScheduledExternalProgramTask(luigi.Task, ScheduledTask):
     """
     Variant of :class:`luigi.contrib.external_program.ExternalProgramTask` that
     executes the task with a :class:`Scheduler`.
     """
     scheduler: str = luigi.ChoiceParameter(default=cfg.scheduler,
-                                           choices=['local'] + [blurb for blurb in get_available_schedulers()],
+                                           choices=[blurb for blurb in get_available_schedulers()],
                                            positional=False, significant=False,
-                                           description='Scheduler to use for running the task')
+                                           description='Scheduler to use for running the task (defaults to bioluigi.scheduler)')
     scheduler_partition: Optional[str] = luigi.OptionalParameter(default=cfg.scheduler_partition, positional=False,
                                                                  significant=False,
-                                                                 description='Scheduler partition (or queue) to use if supported')
+                                                                 description='Scheduler partition (or queue) to use if supported (defaults to bioluigi.scheduler_partition)')
     scheduler_extra_args: list[str] = luigi.ListParameter(default=cfg.scheduler_extra_args, positional=False,
                                                           significant=False,
-                                                          description='Extra arguments to pass to the scheduler')
+                                                          description='Extra arguments to pass to the scheduler (defaults to bioluigi.scheduler_extra_args)')
 
     walltime: timedelta = luigi.TimeDeltaParameter(default=datetime.timedelta(), positional=False, significant=False,
                                                    description='Amount of time to allocate for the task, default value of zero implies unlimited time')
@@ -42,21 +42,17 @@ class ScheduledExternalProgramTask(ExternalProgramTask, ScheduledTask):
     memory: float = luigi.FloatParameter(default=1, positional=False, significant=False,
                                          description='Amount of memory (in gigabyte) to allocate for the task')
 
+    working_directory = luigi.OptionalParameter(default=None, significant=False, positional=False)
+
+    capture_output = luigi.BoolParameter(default=True, significant=False, positional=False)
+
+    def program_environment(self) -> dict[str, str]:
+        env = os.environ.copy()
+        return env
+
     @property
     def resources(self):
-        if self.scheduler == 'local':
-            # local_jobs is actually constrained by the number of workers
-            return {'cpus': self.cpus, 'memory': self.memory}
-        else:
-            return {'{}_jobs'.format(self.scheduler): 1, '{}_cpus'.format(self.scheduler): self.cpus}
+        return get_scheduler(self.scheduler).get_resources_for_task(self)
 
     def run(self):
-        if self.scheduler == 'local':
-            super().run()
-        else:
-            try:
-                _scheduler = get_scheduler(self.scheduler)
-            except KeyError:
-                raise ValueError('Unsupported scheduler {}'.format(self.scheduler))
-            _scheduler.run_task(self)
-
+        get_scheduler(self.scheduler).run_task(self)
