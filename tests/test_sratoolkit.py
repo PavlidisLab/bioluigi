3ebf9950010f96acbe977afaf70be4918fc3b555
Guillaume Poirier-Morency <poirigui@msl.ubc.ca>
2025-10-23 11:01:31 -0700

    sratoolkit: Simplify output of FastqDump to simply be the output directory

--- tests/test_sratoolkit.py
+++ tests/test_sratoolkit.py
@@ -3,6 +3,7 @@ from bioluigi.tasks import sratoolkit
 def test_prefetch():
     prefetch_task = sratoolkit.Prefetch('srr_accession', 'outdir')
     args = prefetch_task.program_args()
+    assert args == ['prefetch', '--max-size', '20G', '--output-file', 'outdir', 'srr_accession']
     assert 'cpus' in prefetch_task.resources
     assert 'prefetch_jobs' in prefetch_task.resources
     assert prefetch_task.resources['prefetch_jobs'] == 1
@@ -10,10 +11,10 @@ def test_prefetch():
 def test_fastq_dump():
     fastq_dump_task = sratoolkit.FastqDump('srr_accession', 'outdir', minimum_read_length=18)
     args = fastq_dump_task.program_args()
-    assert len(fastq_dump_task.output()) == 3
+    assert fastq_dump_task.output().path == 'outdir'
     assert '--split-3' in args
     assert '-M' in args
-    assert 18 in args
+    assert '18' in args
     assert args[-3] == '--outdir'
     assert args[-2] == 'outdir'
     assert args[-1] == 'srr_accession'
@@ -21,13 +22,12 @@ def test_fastq_dump():
     assert fastq_dump_task.resources['fastq_dump_jobs'] == 1
 
 def test_fastq_dump_split_files():
-    fastq_dump_task = sratoolkit.FastqDump('srr_accession', 'outdir', split='files', minimum_read_length=18,
-                                           number_of_reads_per_spot=4)
+    fastq_dump_task = sratoolkit.FastqDump('srr_accession', 'outdir', split='files', minimum_read_length=18)
     args = fastq_dump_task.program_args()
     assert '--split-files' in args
-    assert len(fastq_dump_task.output()) == 4
+    assert fastq_dump_task.output().path == 'outdir'
     assert '-M' in args
-    assert 18 in args
+    assert '18' in args
     assert args[-3] == '--outdir'
     assert args[-2] == 'outdir'
     assert args[-1] == 'srr_accession'
@@ -39,7 +39,7 @@ def test_fastq_dump_split_spot():
     args = fastq_dump_task.program_args()
     assert '--split-spot' in args
     assert '--skip-technical' in args
-    assert len(fastq_dump_task.output()) == 1
+    assert fastq_dump_task.output().path == 'outdir'
     assert args[-3] == '--outdir'
     assert args[-2] == 'outdir'
     assert args[-1] == 'srr_accession'
